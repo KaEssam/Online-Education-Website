2c036107077bd01adf723c46b34e61bc3ae20ba0
ola-elnakeib <olaelnakeib@gmail.com>
2024-03-14 17:11:24 +0200

    Add Header And Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SignInComponent } from './Core/sign-in/sign-in.component';
 import { SignUpComponent } from './Core/sign-up/sign-up.component';
+import { HeaderComponent } from '../Components/Layout/header/header.component';
+import { FooterComponent } from '../Components/Layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,SignInComponent,SignUpComponent],
+  imports: [RouterOutlet,SignInComponent,SignUpComponent,HeaderComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
