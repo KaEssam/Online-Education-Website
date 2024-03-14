86ce55b709ec3656c1bd224db7a8f899ce5fa182
KaEssam <Kaessam17@gmail.com>
2024-03-14 16:47:20 +0200

    Create Sign-in & Sign-up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SignInComponent } from './Core/sign-in/sign-in.component';
+import { SignUpComponent } from './Core/sign-up/sign-up.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SignInComponent,SignUpComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
