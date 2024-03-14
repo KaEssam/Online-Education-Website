c04aea66bae8faef7d40c38ee3dcc9ec78fe6548
KaEssam <Kaessam17@gmail.com>
2024-03-14 17:41:07 +0200

    Edit Sign-in & Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SignInComponent } from './Core/sign-in/sign-in.component';
-import { SignUpComponent } from './Core/sign-up/sign-up.component';
 import { HeaderComponent } from '../Components/Layout/header/header.component';
 import { FooterComponent } from '../Components/Layout/footer/footer.component';
+import { SignInComponent } from '../Components/Core/sign-in/sign-in.component';
+import { SignUpComponent } from '../Components/Core/sign-up/sign-up.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,SignInComponent,SignUpComponent,HeaderComponent,FooterComponent],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent,SignInComponent,SignUpComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
