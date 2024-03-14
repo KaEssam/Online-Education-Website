6e3ff87f48926744a99343e8c233a2a388fbe6b8
KaEssam <Kaessam17@gmail.com>
2024-03-15 01:33:54 +0200

    Create About Us Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,12 @@ import { SignInComponent } from '../Components/Core/sign-in/sign-in.component';
 import { SignUpComponent } from '../Components/Core/sign-up/sign-up.component';
 import { ErrorPageComponent } from '../Components/Core/error-page/error-page.component';
 import { ContactUsComponent } from '../Components/Core/contact-us/contact-us.component';
+import { AboutUsComponent } from '../Components/Core/about-us/about-us.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,FooterComponent,SignInComponent,SignUpComponent,ErrorPageComponent,ContactUsComponent],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent,SignInComponent,SignUpComponent,ErrorPageComponent,ContactUsComponent,AboutUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
