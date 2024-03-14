1990a9f3b3313bcbe3fcddd2cc9c477db8cd30de
KaEssam <Kaessam17@gmail.com>
2024-03-15 01:58:42 +0200

    Fix Dropdown List For Header

--- src/Components/Layout/header/header.component.ts
+++ src/Components/Layout/header/header.component.ts
@@ -1,12 +1,36 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+isDropdownOpen = false;
+  isSubmenuOpen: string | null = null;
 
+  toggleDropdown(event: Event): void {
+    this.isDropdownOpen = !this.isDropdownOpen;
+    event.preventDefault();
+  }
+
+  toggleSubmenu(event: Event, submenu: string): void {
+    this.isSubmenuOpen = this.isSubmenuOpen === submenu ? null : submenu;
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  areNotificationsOpen = false;
+  isAvatarDropdownOpen = false;
+
+  toggleNotifications(): void {
+    this.areNotificationsOpen = !this.areNotificationsOpen;
+  }
+
+  toggleAvatarDropdown(): void {
+    this.isAvatarDropdownOpen = !this.isAvatarDropdownOpen;
+  }
 }
