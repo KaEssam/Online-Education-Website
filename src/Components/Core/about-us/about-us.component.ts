8931d341f8b27ef2ec10cf811436f5cfc8f343dd
ola-elnakeib <olaelnakeib@gmail.com>
2024-03-17 02:36:04 +0200

    Make style for about us component + install owl-carousel

--- src/Components/Core/about-us/about-us.component.ts
+++ src/Components/Core/about-us/about-us.component.ts
@@ -1,12 +1,40 @@
+import { RouterLink } from '@angular/router';
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,CommonModule,CarouselModule],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })
 export class AboutUsComponent {
-
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 600,
+    navText: ['&#8249', '&#8250;'],
+    responsive: {
+      0: {
+        items: 1 
+      },
+      400: {
+        items: 2
+      },
+      760: {
+        items: 3
+      },
+      1000: {
+        items: 4
+      }
+    },
+    nav: true
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,14 @@ import { SignUpComponent } from '../Components/Core/sign-up/sign-up.component';
 import { ErrorPageComponent } from '../Components/Core/error-page/error-page.component';
 import { ContactUsComponent } from '../Components/Core/contact-us/contact-us.component';
 import { AboutUsComponent } from '../Components/Core/about-us/about-us.component';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,FooterComponent,SignInComponent,SignUpComponent,ErrorPageComponent,ContactUsComponent,AboutUsComponent],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent,SignInComponent,SignUpComponent,ErrorPageComponent,ContactUsComponent,AboutUsComponent,CommonModule,CarouselModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
