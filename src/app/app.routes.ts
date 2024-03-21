4d809f9cb5dab84a8eb8c9a15272441e16c1f0ce
KaEssam <Kaessam17@gmail.com>
2024-03-21 22:06:54 +0200

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
+import { SignUpComponent } from "./../Components/Core/sign-up/sign-up.component";
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: "signup", component:SignUpComponent},
+];
