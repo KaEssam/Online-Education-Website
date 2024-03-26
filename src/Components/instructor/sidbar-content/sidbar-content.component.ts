4e9e6134434624c7b19298857131bd168d0fd6d8
KaEssam <Kaessam17@gmail.com>
2024-03-26 07:38:34 +0200

    create content sidebar for instructor dashbord

---
+++ src/Components/instructor/sidbar-content/sidbar-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidbar-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidbar-content.component.html',
+  styleUrl: './sidbar-content.component.css'
+})
+export class SidbarContentComponent {
+
+}
