1dadf74d1dbe52b6f2998fa12c7c6f54cf2a3f7e
KaEssam <Kaessam17@gmail.com>
2024-03-26 07:38:05 +0200

    create sidebar for instructor dashbord

---
+++ src/Components/instructor/instructor-sidebar/instructor-sidebar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-instructor-sidebar',
+  standalone: true,
+  imports: [],
+  templateUrl: './instructor-sidebar.component.html',
+  styleUrl: './instructor-sidebar.component.css'
+})
+export class InstructorSidebarComponent {
+
+  
+}
