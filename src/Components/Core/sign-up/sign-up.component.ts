3246e9281d6026f3b41a41e6b56cba261a104f7c
Deyaa Taha <deyaa.taha.0@gmail.com>
2024-03-28 07:47:29 +0200

    general fixes

--- src/Components/Core/sign-up/sign-up.component.ts
+++ src/Components/Core/sign-up/sign-up.component.ts
@@ -19,8 +19,8 @@ constructor(private fb: FormBuilder, private router: Router, private authService
   signupForm = this.fb.group({
     firstName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20), Validators.pattern('^[a-zA-Z]+$')]],
     lastName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20), Validators.pattern('^[a-zA-Z]+$')]],
-    email: ['', [Validators.required, Validators.email, Validators.maxLength(50), Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$')]],
-    password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(20),]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(20), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9@!_.%&]+$')]],
     confirmPassword: ['', Validators.required]
   }, { validators: this.passwordMatchValidator });
 
@@ -57,8 +57,8 @@ constructor(private fb: FormBuilder, private router: Router, private authService
   validateAndSubmit() {
     if (this.signupForm.valid) {
       const { firstName, lastName, email, password } = this.signupForm.value;
-      this.addUser(firstName, lastName, email, password);
-      this.router.navigate(['/signin']);
+      // this.addUser(firstName, lastName, email, password);
+      // this.router.navigate(['/signin']);
       
       this.addUser(firstName, lastName, email, password);
       this.router.navigate(['/login']);
@@ -72,5 +72,3 @@ constructor(private fb: FormBuilder, private router: Router, private authService
     this.authService.SignUp(newUser).subscribe();
   }
 }
-
-
