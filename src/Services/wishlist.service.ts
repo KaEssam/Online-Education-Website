18391a500e74dbce74c1c32ec54f08de0ed400c3
Deyaa Taha <deyaa.taha.0@gmail.com>
2024-03-30 06:35:10 +0200

    Updated auth to allow calling authorized endpoints

--- src/Services/wishlist.service.ts
+++ src/Services/wishlist.service.ts
@@ -2,22 +2,28 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WishlistService {
-  private readonly WISH_URL = "http://localhost:3000/wishlist"; // Separate URL for cart operations
-  constructor(private readonly Client: HttpClient) { }
+  private readonly WISH_URL = 'https://skillgro.runasp.net/api/WishList'; // Separate URL for cart operations
+  constructor(private readonly Client: HttpClient) {}
 
   addToWish(product: any) {
     return this.Client.post(this.WISH_URL, product); // Post product to the cart URL
   }
 
-  getWishItems(){
-    return this.Client.get(this.WISH_URL); // Fetch items from the cart URL
+  getWishItems() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+      }),
+    };
+
+    return this.Client.get(this.WISH_URL, httpOptions); // Fetch items from the cart URL
   }
 
   deleteFromCart(id: any) {
-    return this.Client.delete(this.WISH_URL + "/" + id);
+    return this.Client.delete(this.WISH_URL + '/' + id);
   }
-
 }
