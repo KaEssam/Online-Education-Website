bc8b429b17bb229d058dc67633738d563f1d9102
KaEssam <Kaessam17@gmail.com>
2024-04-03 09:09:49 +0200

    create student enroll content &style

---
+++ src/Components/Student/student-content/student-content/student-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './student-content.component.html',
+  styleUrl: './student-content.component.css'
+})
+export class StudentContentComponent {
+
+}
