0f212cd5341f09820252c446fa395f1a96b02169
KaEssam <Kaessam17@gmail.com>
2024-04-08 20:39:54 +0200

    update user name for instructor

--- src/Components/instructor/instructor-sidebar/instructor-sidebar.component.ts
+++ src/Components/instructor/instructor-sidebar/instructor-sidebar.component.ts
@@ -1,23 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CreateCourseService } from '../../../Services/create-course.service';
+import { UserInfoService } from '../../../Services/user-info.service';
 
 @Component({
   selector: 'app-instructor-sidebar',
   standalone: true,
   imports: [RouterModule],
-  providers: [CreateCourseService],
+  providers: [CreateCourseService,UserInfoService],
   templateUrl: './instructor-sidebar.component.html',
   styleUrl: './instructor-sidebar.component.css'
 })
 export class InstructorSidebarComponent {
 
-  constructor(private createCourseService:CreateCourseService) { }
+  constructor(private createCourseService:CreateCourseService, private UserInfoService: UserInfoService) { }
 
   Courses: any = [];
+  user: any;
 
   ngOnInit(): void {
     this.getAllCreatedCourses();
+    this.viewUserInfo();
+  }
+
+      viewUserInfo() {
+    this.UserInfoService.getUserInfo().subscribe({
+      next: (data) => {
+        this.user = data;
+        console.log(data);
+      },
+      error: (err) => {
+        console.log(err.message);
+      }
+    })
   }
 
   getAllCreatedCourses() {
