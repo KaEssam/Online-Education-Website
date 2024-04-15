5453074e4e7f0df03f77ea01bd74ec99ab2d9869
Deyaa Taha <deyaa.taha.0@gmail.com>
2024-04-15 19:23:39 +0200

    made update button in instructor dashboard return the selected course data

--- src/Components/instructor/sidbar-content/sidbar-content.component.ts
+++ src/Components/instructor/sidbar-content/sidbar-content.component.ts
@@ -1,82 +1,76 @@
-import { UserInfoService } from "./../../../Services/user-info.service";
-import { CreateCourseService } from "./../../../Services/create-course.service";
+import { UserInfoService } from './../../../Services/user-info.service';
+import { CreateCourseService } from './../../../Services/create-course.service';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import Swal from "sweetalert2";
-
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-sidbar-content',
   standalone: true,
   imports: [RouterModule],
-  providers: [CreateCourseService,],
+  providers: [CreateCourseService],
   templateUrl: './sidbar-content.component.html',
-  styleUrl: './sidbar-content.component.css'
+  styleUrl: './sidbar-content.component.css',
 })
 export class SidbarContentComponent {
-
-  constructor(private createCourseService:CreateCourseService, ) { }
+  constructor(private createCourseService: CreateCourseService) {}
 
   Courses: any = [];
   user: any;
 
-
-
   ngOnInit(): void {
     this.getAllCreatedCourses();
-
   }
-
-
 
   getAllCreatedCourses() {
     this.createCourseService.getAllCourses().subscribe({
       next: (data) => {
         console.log(data);
         this.Courses = data;
-        
       },
       error: (err) => {
         console.log(err);
-      }
+      },
     });
-}
-
-deleteCourse(id: number) {
- Swal.fire({
-    title: 'Are you sure?',
-    text: "This action will remove the content from the current view.",
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Yes, delete it!'
-  }).then((result) => {
-if (result.isConfirmed) {
-      this.createCourseService.deleteCourse(id).subscribe({
-    next: (data) => {
-      console.log(data);
-      this.getAllCreatedCourses();
-      Swal.fire(
-          'Deleted!',
-          'Your content has been removed (temporarily).',
-          'success'
-        );
-    },
-    error: (err) => {
-      console.log(err);
-    }
-  });
-    }
-  });
-  
   }
 
+  deleteCourse(id: number) {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'This action will remove the content from the current view.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.createCourseService.deleteCourse(id).subscribe({
+          next: (data) => {
+            console.log(data);
+            this.getAllCreatedCourses();
+            Swal.fire(
+              'Deleted!',
+              'Your content has been removed (temporarily).',
+              'success'
+            );
+          },
+          error: (err) => {
+            console.log(err);
+          },
+        });
+      }
+    });
+  }
 
-  updateCourse(id: number) {
-
-
+  loadCourseToUpdate(id: number) {
+    this.createCourseService.getCourseById(id).subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
-
-}
-
